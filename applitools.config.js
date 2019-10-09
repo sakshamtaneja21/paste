e754ed65f9c49b783f464a33cee1441eb2469fd6
Si Taggart <me@simontaggart.com>
2019-10-09 15:03:49 -0700

    fix: circle config

--- applitools.config.js
+++ applitools.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   serverUrl: 'https://twilioeyesapi.applitools.com',
+  browser: [{width: 800, height: 600, name: 'chrome'}, {width: 800, height: 600, name: 'ie11'}],
   batchId: process.env.APPLITOOLS_BATCH_ID,
+  branchName: process.env.CIRCLE_BRANCH,
+  baselineBranchName: 'master',
+  exitcode: true,
+  showLogs: true,
+  saveDebugData: true,
 };
