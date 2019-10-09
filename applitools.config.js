318caaff3548c852ce7f69de4f9a0883c44ff20e
Si Taggart <me@simontaggart.com>
2019-10-09 15:51:49 -0700

    chore: one more

--- applitools.config.js
+++ applitools.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   serverUrl: 'https://twilioeyesapi.applitools.com',
-  browser: [{width: 800, height: 600, name: 'chrome'}, {width: 800, height: 600, name: 'ie11'}],
+  // browser: [{width: 800, height: 600, name: 'chrome'}, {width: 800, height: 600, name: 'ie11'}],
   batchId: process.env.APPLITOOLS_BATCH_ID,
   branchName: process.env.CIRCLE_BRANCH,
   baselineBranchName: 'master',
