f6f4c28213615789af3c3cd975ae215667cbb5ee
Si Taggart <me@simontaggart.com>
2019-10-09 17:16:28 -0700

    chore: branch names don't seem to work

--- applitools.config.js
+++ applitools.config.js
@@ -2,8 +2,6 @@ module.exports = {
   serverUrl: 'https://twilioeyesapi.applitools.com',
   // browser: [{width: 800, height: 600, name: 'chrome'}, {width: 800, height: 600, name: 'ie11'}],
   batchId: process.env.APPLITOOLS_BATCH_ID,
-  branchName: process.env.CIRCLE_BRANCH,
-  // baselineBranchName: 'master',
   exitcode: 0,
   showLogs: true,
 };
